7404e63b00fc0d2f2717d028da9bb9111be0728a
Ankit Gaur <iamankitgaur@gmail.com>
2023-01-04 23:33:22 +0530

    final project ready only fonts and colour to be added

--- apis/general_pages/forms_data.py
+++ apis/general_pages/forms_data.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pickle
 
-Model = pickle.load(open("MachineModels/Laptop Prize prediction/LaptopPriceModel.pkl","rb"))
+Model = pickle.load(open("MachineModels\Laptop Prize prediction\LaptopPriceModel.pkl","rb"))
+Model2 = pickle.load(open("MachineModels\Calories Burnt Prediction\CalBurnModel.pkl","rb"))
+Model3 = pickle.load(open("MachineModels\Diabetes prediction\DiabeteseModel.pkl","rb"))
 
 
 def laptoppricecal(Company: str, TypeName: str, Ram:int, Weight: float,TouchScreen: int,IPS: int,ScreenSize: int,Resolution: str,Cpu_brand: str,HDD: int,SSD: int,Gpu_brand: str,os: str):
@@ -13,7 +15,19 @@ def laptoppricecal(Company: str, TypeName: str, Ram:int, Weight: float,TouchScre
     result = str(int(np.exp(Model.predict(query)[0])))
     return result
 
-# def caloriesburncal():
-#     pass
-# def diabetesecal():
-#     pass
+def caloriesburncal(gender: int, age: int,height: float, Weight: float, duration: float, heartrate: float, bodytemp: float):
+# Gender	Age	Height	Weight	Duration	Heart_Rate	Body_Temp
+    query = np.array([gender,age,height,Weight,duration,heartrate,bodytemp])
+    query = query.reshape(1,7)
+    result = str(round(Model2.predict(query)[0], 3))
+    return result
+
+def diabetesecal(Pregnancies:int, Glucose:int, BloodPressure:int, SkinThickness:int, Insulin:int, BMI:float, DPFunction:float,	Age:int):
+#  Pregnancies	Glucose	BloodPressure	SkinThickness	Insulin	BMI	DiabetesPedigreeFunction	Age	Outcome
+    query = np.array([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DPFunction,Age])
+    query = query.reshape(1,8)
+    if Model3.predict(query)[0]==0:
+        result = "Non - Diabetic" 
+    else:
+        result = "Diabetic"
+    return result
--- apis/general_pages/route_homepage.py
+++ apis/general_pages/route_homepage.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from apis.general_pages.forms_data import laptoppricecal
+from apis.general_pages.forms_data import laptoppricecal,caloriesburncal,diabetesecal
 from pydantic import ValidationError
 
 templates = Jinja2Templates(directory="templates")
@@ -14,33 +14,50 @@ async def home(request:Request):
 
 @general_pages_router.get("/caloriesburnt")
 async def caloriesburnt(request:Request):
-    return templates.TemplateResponse("gernal_pages/caloriesburntpred.html",{"request":request})
+    return templates.TemplateResponse("gernal_pages/caloriesburntpred.html",{"request":request,"Result":-1})
 
 @general_pages_router.get("/diabetese")
 async def diabetese(request:Request):
-    return templates.TemplateResponse("gernal_pages/diabetesepred.html",{"request":request})
+    return templates.TemplateResponse("gernal_pages/diabetesepred.html",{"request":request,"Result":""})
 
 @general_pages_router.get("/laptopprice")
 async def laptopprice(request:Request):
     return templates.TemplateResponse("gernal_pages/laptoppricepred.html",{"request":request,"Result":-1})
 
-# @general_pages_router.post("/caloriesburnsubmit")
-# async def caloriesburnsubmit(request:Request):
-#     try:
-#         data = await request.form()
+@general_pages_router.post("/caloriesburnsubmit")
+async def caloriesburnsubmit(request:Request):
+    try:
+        data = await request.form()
+#   {"Gender":"1","Age":"0","Height":"0","Weight":"0","Duration":"0","HeartRate":"0","BodyTemp":"0"}
+        Gender = int(data["Gender"]) 
+        Age = int(data["Age"]) 
+        Height = float(data["Height"])
+        Weight = float(data["Weight"])
+        Duration = float(data["Duration"])
+        HeartRate = float(data["HeartRate"])
+        BodyTemp = float(data["BodyTemp"])
+        result = caloriesburncal(Gender,Age,Height,Weight,Duration,HeartRate,BodyTemp) 
+        return templates.TemplateResponse("gernal_pages/caloriesburntpred.html",{"request":request,"Result":result})
+    except ValidationError as e:
+        return {"message": "Invalid input"+e }
 
-#         return templates.TemplateResponse("gernal_pages/laptoppricepred.html",{"request":request,"Result":price})
-#     except ValidationError as e:
-#         return {"message": "Invalid input"+e }
-
-# @general_pages_router.post("/diabetesesubmit")
-# async def caloriesburnsubmit(request:Request):
-#     try:
-#         data = await request.form()
-
-#         return templates.TemplateResponse("gernal_pages/laptoppricepred.html",{"request":request,"Result":price})
-#     except ValidationError as e:
-#         return {"message": "Invalid input"+e }
+@general_pages_router.post("/diabetesesubmit")
+async def caloriesburnsubmit(request:Request):
+    try:
+        data = await request.form()
+        # Pregnancies:int, Glucose:int, BloodPressure:int, SkinThickness:int, Insulin:int, BMI:float, DPFunction:float,	Age:int
+        Pregnancies = int(data["Pregnancies"])
+        Glucose=int(data["Glucose"])
+        BloodPressure=int(data["BloodPressure"])
+        SkinThickness=int(data["SkinThickness"])
+        Insulin=int(data["Insulin"])
+        BMI=float(data["BMI"])
+        DPFunction=float(data["DPF"])
+        Age=int(data["Age"])
+        result = diabetesecal(Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DPFunction,Age)
+        return templates.TemplateResponse("gernal_pages/diabetesepred.html",{"request":request,"Result":result})
+    except ValidationError as e:
+        return {"message": "Invalid input"+e }
 
 
 @general_pages_router.post("/laptoppricepredsubmit")
